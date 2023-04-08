5769682c87bfb560a8fa4346a6952c24cc7e49cd
muhammadamas <amaswaseem@gmail.com>
2023-04-09 04:42:45 +0500

    schema updated

--- server/schema.sql
+++ server/schema.sql
@@ -1,3 +1,6 @@
+## Create your DataBase
+### Here is the Schema code
+```sql
 CREATE DATABASE record_keeper_app;
 USE record_keeper_app;
 
@@ -12,4 +15,14 @@ INSERT INTO records (title, contents)
 VALUES 
 ('Meeting notes', 'Discussed upcoming project timelines and resource allocation.'),
 ('Shopping list', 'Milk, eggs, bread, cheese, and apples.'),
-('Fitness log', 'Ran 5 miles in 40 minutes. Did 3 sets of 10 reps for bench press.');
+('Fitness log', 'Ran 5 miles in 40 minutes. Did 3 sets of 10 reps for bench press.'),
+('To-do list', 'Call the bank, schedule a dentist appointment, and buy a birthday present for Sarah.'),
+('Recipes', 'Spaghetti Bolognese, Shepherd's Pie, and Chocolate Brownies.');
+
+```
+## Change the configuration in `.env` file
+## Run the following commands
+```javascript
+$ npm install
+$ npm run dev
+```
